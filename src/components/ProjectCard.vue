a7a19c0230a5e44f42da6d5c10017490ba6c0c0c
antden99 <antoniodenigris@icloud.com>
2024-05-30 18:38:49 +0200

    final commit

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: ['project','url_Img'],
+    props: ['project', 'url_Img'],
 
     data() {
         return {
@@ -12,13 +12,11 @@ export default {
 </script>
 
 <template>
-    <div class="card">
-        <template v-if="project.cover_image.startsWith('uploads')">
-            <img :src="url_Img + 'storage/' + project.cover_image" alt="">
-        </template>
-        <template v-else>
-            <img :src="project.cover_image" alt="">
-        </template>
+    <div class="card ">
+        <img :src="url_Img + 'storage/' + project.cover_image" alt="" v-if="project.cover_image.startsWith('uploads')"
+            class="card-img-top">
+        <img :src="project.cover_image" alt="" v-else class="card-img-top">
+
         <div class="card-body">
             <h5 class="card-title">{{ project.name }}</h5>
             <div class="card-text">{{ project.start_date }}</div>
@@ -26,4 +24,12 @@ export default {
     </div>
 </template>
 
-<style></style>
+<style>
+
+.card-img-top {
+    width: 100%;
+    height: 300px; 
+    object-fit: cover;
+    object-position: center;
+}
+</style>
