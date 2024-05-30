4fc61914bf6f61d427cb6116693d771b685011d7
antden99 <antoniodenigris@icloud.com>
2024-05-30 16:45:46 +0200

    add milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
 import axios from 'axios';
+import ProjectCard from '../src/components/ProjectCard.vue';
 
 export default {
 
@@ -7,18 +8,23 @@ export default {
   data() {
     return {
       message: "Ciao Anto",
-      projectList:[],
-      base_api:'http://127.0.0.1:8000/',
-      name_api:'api/projects',
+      projectList: [],
+      base_api: 'http://127.0.0.1:8000/',
+      name_api: 'api/projects',
     }
+  },
+
+  components: {
+    ProjectCard,
   },
 
   mounted() {
 
     axios.get(this.base_api + this.name_api)
       .then(response => { //quando ottengo response, svolgo la funzione 
-        //console.log(response.data);
-        this.projectList = response.data; //salvo nell'array dichiarato in data, il risultato ottenuto dalla chiamata ajax
+        console.log(response.data);
+        this.projectList = response.data.projects; //salvo nell'array dichiarato in data, il risultato ottenuto dalla chiamata ajax
+        //console.log( this.projectList);
       })
       .catch(error => {
         console.error('Errore durante la chiamata API:', error);
@@ -30,7 +36,23 @@ export default {
 </script>
 
 <template>
-  <div>{{ message }} </div>
+  <h1>Vite-boolfolio</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-4 gy-3" v-for="project in projectList.data">
+        <ProjectCard :project="project"></ProjectCard>
+      </div>
+      <nav aria-label="...">
+        <ul class="pagination pagination-lg">
+          <li class="page-item active" aria-current="page">
+            <span class="page-link">1</span>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object,
+    },
+
+    data() {
+        return {
+        }
+    },
+}
+
+</script>
+
+<template>
+    <div class="card">
+        <img :src="project.cover_image" class="card-img-top" alt="Card image">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.name }}</h5>
+            <div class="card-text">{{ project.start_date }}</div>
+        </div>
+    </div>
+</template>
+
+<style></style>
