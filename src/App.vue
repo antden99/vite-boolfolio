99b8d6525653276ccf84f81188642a0db777f3c1
antden99 <antoniodenigris@icloud.com>
2024-05-30 15:24:14 +0200

    add milestone 3 e milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
-<script setup>
+<script>
+import axios from 'axios';
+
+export default {
+
+  name: 'App',
+  data() {
+    return {
+      message: "Ciao Anto",
+      projectList:[],
+      base_api:'http://127.0.0.1:8000/',
+      name_api:'api/projects',
+    }
+  },
+
+  mounted() {
+
+    axios.get(this.base_api + this.name_api)
+      .then(response => { //quando ottengo response, svolgo la funzione 
+        //console.log(response.data);
+        this.projectList = response.data; //salvo nell'array dichiarato in data, il risultato ottenuto dalla chiamata ajax
+      })
+      .catch(error => {
+        console.error('Errore durante la chiamata API:', error);
+      }); //aggiungo la catach per la gestione degli errori
+  }
+}
+
 
 </script>
 
 <template>
-  ciao
+  <div>{{ message }} </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
